fd4e404e661abc72cd61307748c5ed5a144ad049
Noah Kleiner <noah@njkleiner.com>
2020-12-24 14:59:58 +0100

    Add some documentation

--- confusion.go
+++ confusion.go
@@ -1,3 +1,5 @@
+// Package confusion provides a simple mechanism for loading configuration
+// files of arbitrary formats from a number of alternate locations.
 package confusion
 
 import (
@@ -10,10 +12,21 @@ import (
 )
 
 var (
+	// ErrConfigNotFound is returned when LoadConfig is unable to locate
+	// a suitable configuration file in any of the provided locations.
 	ErrConfigNotFound = errors.New("config not found")
+	// ErrLoaderNotFound is returned by LoadConfig if a configuration
+	// file is found but it cannot be loaded because no Loader matching
+	// its file extension is available.
 	ErrLoaderNotFound = errors.New("loader not found")
 )
 
+// Options determines where LoadConfig will look for configuration files
+// and what formats can be loaded.
+//
+// LoadConfig will first consider the UserPaths before falling back to
+// SystemPaths. Loaders maps file extensions (e.g., ".json") to a Loader
+// that is capable of loading configuration files with this extension.
 type Options struct {
 	Prefix      string
 	UserPaths   []string
@@ -23,10 +36,15 @@ type Options struct {
 	fs afero.Fs
 }
 
+// A Loader is responsible for loading configuration files of a specific format.
 type Loader interface {
+	// Load attemtps to load a configuration file from r and store the
+	// result in v.
 	Load(r io.Reader, v interface{}) error
 }
 
+// LoadConfig attempts to load a configuration file with a certain name
+// according to the given Options and store the result in config.
 func LoadConfig(name string, opts Options, config interface{}) error {
 	if opts.fs == nil {
 		opts.fs = afero.NewOsFs()
